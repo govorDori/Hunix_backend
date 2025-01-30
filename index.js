ddaefd2ecd1b7f9a823525b664b65490a8b05bbd
govorDori <dorigovor2006@gmail.com>
2025-01-30 16:07:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import { removeFromCloud } from "./cloudinaryConfig"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+app.delete('', async(req,resp)=>{
+	try {
+		const {id} = req.params
+		removeFromCloud(id)
+		resp.json({msg:"Sikeres törlés!"})
+	} catch (error) {
+		console.log(error);
+		
+	}
+})
+
+app.listen(port,()=>console.log("Server listening on port: " + port))
