7688073fddbd5ad4bcb92b77635ec6de10518aaf
Your Name <you@example.com>
2025-01-31 11:39:06 +0100

    import fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import cors from "cors"
-import { removeFromCloud } from "./cloudinaryConfig"
+import { removeFromCloud } from "./cloudinaryConfig.js"
 
 const app = express()
 app.use(cors())
